54bc568de66cc852c5c5a96e724f5ffdaf9c2a6b
lucas <lucas.s.s.silva@hotmail.com>
2020-09-01 17:38:50 -0300

    constraint

--- aula_02.sql
+++ aula_02.sql
@@ -3,13 +3,13 @@
 	
 */
 
---Excluindo chave estrangeira(relação entre uma tabela e outra) da tabela vendaProd
+--Excluindo chave estrangeira(relaï¿½ï¿½o entre uma tabela e outra) da tabela vendaProd
 alter table vendaProd drop constraint [FK__vendaProd__cd_ve__3A81B327] -- FK excluida
 
---Excluindo a chave primária da tabela venda
-alter table venda drop constraint [PK__venda__8C6EA002DD3156E2] -- PK excluída
+--Excluindo a chave primï¿½ria da tabela venda
+alter table venda drop constraint [PK__venda__8C6EA002DD3156E2] -- PK excluï¿½da
 
---Recriando chave primária da tabela venda
+--Recriando chave primï¿½ria da tabela venda
 alter table venda add constraint PK_Venda Primary key (cd_venda) 
 
 --Recriando o relacionamento entre a tabela venda e vendaProd
@@ -24,17 +24,16 @@ alter table vendaProd drop constraint [FK__vendaProd__cd_pr__5CD6CB2B]
 --Excluir primary key da tabela produto
 alter table produto drop constraint [PK__produto__5D5AA9F3BDD4352F]
 
---Criando chave primária da tabela produto
+--Criando chave primï¿½ria da tabela produto
 alter table produto add constraint PK_Produto primary key (cd_produto)
 
 --Recriando relacionamento entre a tabela produto e vendaProd
 alter table vendaProd add constraint FK_Produto foreign key (cd_produto) references produto
 
 
--- Adicionar um check na tabela produto para o vl_produto == adicionar uma restrição 
-alter table produto add constraint CK_vlProduto check (vl_produto > 5) -- já com o nome definido
+-- Adicionar um check na tabela produto para o vl_produto == adicionar uma restriï¿½ï¿½o 
+alter table produto add constraint CK_vlProduto check (vl_produto > 5) -- jï¿½ com o nome definido
 
 
 --Alterar a coluna vl_produto da tabela produto para not null
 alter table produto alter column vl_produto money not null 
-
